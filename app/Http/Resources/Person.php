d49c48f2c6876abd6a8f13daeb0d34f2bad19768
Winner <bihames4vainqueur@gmail.com>
2022-01-28 06:51:53 +0200

    people: resource - add a resource to format people

--- app/Http/Resources/Person.php
+++ app/Http/Resources/Person.php
@@ -14,6 +14,12 @@ class Person extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id" => 121,
+            "first_name" =>  $this->first_name,
+            "last_name" =>  $this->last_name,
+            "email" => $this->email,
+            "dob" => $this->dob,
+        ];
     }
 }
